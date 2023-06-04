f73f60a43679a532cb6ee742896196a52f17d17b
Duy Tran <duymon178@gmail.com>
2023-06-05 00:40:59 +0700

    Fix failed test

--- src/app/user-list-search/user-list-search.component.spec.ts
+++ src/app/user-list-search/user-list-search.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserListSearchComponent } from './user-list-search.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('UserListSearchComponent', () => {
   let component: UserListSearchComponent;
@@ -8,6 +9,7 @@ describe('UserListSearchComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserListSearchComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserListSearchComponent);
