ec8a7672f0f1f3c7bf77b34e535d015e16fe4a6f
Duy Tran <duymon178@gmail.com>
2023-06-04 22:20:22 +0700

    Implement user detail UI

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { UserManagementService } from '../user-management.service';
 
 @Component({
   selector: 'user-management-user-detail',
@@ -6,4 +9,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./user-detail.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserDetailComponent {}
+export class UserDetailComponent {
+  user$ = this.route.params.pipe(
+    switchMap((params) => {
+      return this.service.getUser(+params['id']);
+    })
+  );
+
+  constructor(private service: UserManagementService,
+    private route: ActivatedRoute) {}
+}
